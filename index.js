bc166d6be734d0f73e16cf62929a2cf545bf70d7
Dacr0n <chinmay.hegde2001@gmail.com>
2021-09-09 15:04:10 +0530

    Mediaq_For_landing1.0

---
+++ index.js
@@ -0,0 +1,19 @@
+var item =document.getElementById('item')
+
+item.style.right  = "-50rem";
+function togglemenu(){
+
+    if (item.style.right  == "-50rem") 
+    {
+
+        item.style.right  = "-7rem";
+        
+    } 
+    else 
+    {
+        item.style.right  = "-50rem";
+    }
+    
+    
+
+}
